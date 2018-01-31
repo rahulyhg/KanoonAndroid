0c8768c8a7eabdde3c54dcad9eeb633aaa3a1976
RVishwakarma <ravikant.vishwakarma@technogen.net>
2018-01-31 18:52:23 +0530

    Base Activity Added

---
+++ app/src/main/java/com/rvsoftlab/kanoon/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.rvsoftlab.kanoon;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by RVishwakarma on 1/31/2018.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/rvsoftlab/kanoon/login/LoginActivity.java
+++ app/src/main/java/com/rvsoftlab/kanoon/login/LoginActivity.java
@@ -1,11 +1,11 @@
 package com.rvsoftlab.kanoon.login;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.rvsoftlab.kanoon.BaseActivity;
 import com.rvsoftlab.kanoon.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
