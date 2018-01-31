da8f13942526943fd3cbe2a42c5cd0192f01e58a
rv <email@example.com>
2018-01-31 16:13:03 +0530

    Added Login page
    Signed-off-by: rv <email@example.com>

---
+++ app/src/main/java/rvsoftlab/net/kanoon/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package rvsoftlab.net.kanoon.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rvsoftlab.net.kanoon.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
